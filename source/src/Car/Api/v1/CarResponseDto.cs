bf8e00ea75abb2bf1a3c5e4f9fdd3aff6757d335
angeloluetolf <angelo.luetolf93@gmail.com>
2022-06-21 19:53:28 +0200

    domain model

---
+++ source/src/Car/Api/v1/CarResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CarRent.Car.Api.v1
+{
+    public class CarResponseDto
+    {
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("car-number")]
+        public string CarNumber { get; set; }
+
+        public string Brand { get; set; }
+
+        public string CarClass { get; set; }
+
+        public string Type { get; set; }
+    }
+}
---
+++ source/src/Car/Persistence/CarRepository.cs
@@ -0,0 +1,32 @@
+﻿using CarRent.Car.Domain;
+
+namespace CarRent.Car.Persistence
+{
+    public class CarRepository : ICarRepository
+    {
+        public void Add(Domain.Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Domain.Car GetByCarNumber(string carNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Domain.Car GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Domain.Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Domain.Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
